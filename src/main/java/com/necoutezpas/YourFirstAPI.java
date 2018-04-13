c5f93f5dcb9bae7e060db577b7309039017cda15
Pascal LE LEVIER <pascal.lelevier@gmail.com>
2018-04-13 22:36:32 +0200

    Endpoints AppEngine Skeleton app

---
+++ src/main/java/com/necoutezpas/YourFirstAPI.java
@@ -0,0 +1,12 @@
+package com.necoutezpas;
+
+import com.google.api.server.spi.config.Api;
+
+/**
+  * Add your first API methods in this class, or you may create another class. In that case, please
+  * update your web.xml accordingly.
+ **/
+@Api(name = "skeleton-api",
+     version = "v1")
+public class YourFirstAPI {
+}
